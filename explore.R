be075020c6d84a546fb906479230b4423d0001e3
Nicholas Cotter <nick@extropy.net>
2017-09-19 14:06:28 +0100

    working on date conversion

--- explore.R
+++ explore.R
@@ -1,15 +1,22 @@
-require("dplyr")
-require("lubridate")
+library("dplyr")
+library("lubridate")
+library(data.table)
 
 # function to turn date string into date
 # function to turn date string into time
 # > unlist(strsplit("June 06, 2017 at 09:03AM", "at"))
 
-start <- read.csv("data/august/start.csv")
-expressway <- read.csv("data/august/expressway.csv")
-end <- read.csv("data/august/end.csv")
+start <- fread("data/august/start.csv", select=c(1))
+expressway <- fread("data/august/expressway.csv", select=c(1))
+end <- fread("data/august/end.csv", select=c(1))
+
+# column names
+colnames(start) <- c("DateTime")
+colnames(expressway) <- c("DateTime")
+colnames(end) <- c("DateTime")
 
 # convert date into date and time
+start$DateTime <- as.Date(start$DateTime, format("%b %d, %Y at %H:%M%p"))
 
 # remove outliers (all should be between 6am and 10am)
 
