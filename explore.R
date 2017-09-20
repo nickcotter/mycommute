afaecffd27217d26764667b7e17b7f3e1ba45c7c
Nicholas Cotter <nick@extropy.net>
2017-09-20 13:53:16 +0100

    joined tables by journey date

--- explore.R
+++ explore.R
@@ -18,12 +18,22 @@ colnames(end) <- c("RawDateTime")
 # convert date into date and time
 start$Date <- as.Date(start$RawDateTime, format("%b %d, %Y at %H:%M%p"))
 start$DateTime <- as.POSIXct(start$RawDateTime, format="%b %d, %Y at %H:%M%p")
+start <- subset(start, select=c(2:3))
 
 expressway$Date <- as.Date(expressway$RawDateTime, format("%b %d, %Y at %H:%M%p"))
 expressway$DateTime <- as.POSIXct(expressway$RawDateTime, format="%b %d, %Y at %H:%M%p")
+expressway <- subset(expressway, select=c(2:3))
 
 end$Date <- as.Date(end$RawDateTime, format("%b %d, %Y at %H:%M%p"))
 end$DateTime <- as.POSIXct(end$RawDateTime, format="%b %d, %Y at %H:%M%p")
+end <- subset(end, select=c(2:3))
+
+# join together by date
+
+start_to_expressway <- inner_join(start, expressway, by=c("Date" = "Date"), suffix=c(".start", ".xway"))
+journeys <- inner_join(start_to_expressway, end, by=c("Date" = "Date"))
+colnames(journeys) <- c("Date", "Start", "ExpressWay", "End")
+
 
 # remove outliers (all should be between 6am and 10am)
 
