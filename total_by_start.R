22ca1d1529bf30688b059d3f2ea05bcb49cd0c92
Nicholas Cotter <nick@extropy.net>
2018-04-10 15:56:11 +0100

    removed old now unused files

--- total_by_start.R
+++
@@ -1,6 +0,0 @@
-# plot total journey time by start time
-
-png(file="total_by_start.png")
-p <- ggplot(journeys, aes(StartTime, Total)) + geom_point() + geom_smooth(method="lm")
-print(p)
-dev.off()
--- total_by_start_dow.R
+++
@@ -1,6 +0,0 @@
-# fit total vs start time for different days of the week
-
-png(file="total_by_start_dow.png")
-p <- ggplot(journeys, aes(StartTime, Total)) + geom_point() + geom_smooth(method="lm") + facet_grid(.~dow)
-print(p)
-dev.off()
